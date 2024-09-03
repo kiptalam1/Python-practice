e81297f2b68ba54da2ececa3f4d548e2db47e54c
Adams Kiptalam <adamskiptalam0@gmail.com>
2024-09-03 17:41:36 +0300

    Create TravelManagement.py

---
+++ TravelManagement.py
@@ -0,0 +1,59 @@
+class Travel:
+    def __init__(self, country, month, type):
+        self.country = country
+        self.month = int(month)
+        self.type = type
+        self.price = 0
+
+  
+    def trip_info(self):
+        if self.month >= 10 and self.month <= 3:
+            print(f"You are going to {self.country} in the Winter. This is a {self.type} trip.")
+        elif self.month > 3 and self.month < 10:
+            print(f"You are going to {self.country} in the Summer. This is a {self.type} trip.")
+        else:
+            print("Invalid month!")
+            
+            
+    def calculate_cost(self, cost):
+        costs = []
+        while cost != 0:
+            self.price += cost
+            costs.append(cost)
+            cost = int(input("Enter another cost: "))
+          
+        advice = self.advice(self.price)
+        inspect = self.list_inspect(costs)
+        return advice, inspect
+
+
+    def advice(self, num):
+        if num < 500:
+            print("Low budget.")
+        elif num >= 500 and num < 1500:
+            print("Take a trip anywhere...")
+        else:
+            print("Luxury trip!")
+            
+            
+    def list_inspect(self, costs):
+        less_than_ten = 0
+        for i in costs:
+            if i <= 10:
+                less_than_ten += 1
+        if less_than_ten <= 10:
+            self.price += 100
+            print(f"Updated price: {self.price}")
+            
+
+
+location = input("Enter a country: ").capitalize()
+trip_type = input("Business or Leisure: ").capitalize()
+month = input("Enter month: ")
+
+test = Travel(location, month, trip_type)
+test.trip_info()
+
+flight_cost = int(input("Enter your flight cost: "))
+
+test.calculate_cost(flight_cost)
